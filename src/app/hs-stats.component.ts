3659e277805e8aa380f151c12f2980b3fe54aa7e
Dan <dan.geffroy@capgemini.com>
2016-05-31 15:24:29 +0200

    adding isotope but its so laggy

--- src/app/hs-stats.component.ts
+++ src/app/hs-stats.component.ts
@@ -3,6 +3,7 @@ import { CardsService } from './cards.service';
 import { InfosService } from './infos.service';
 import { Card } from './card';
 import { Infos } from './infos';
+declare var $:any;
 
 @Component({
   moduleId: module.id,
@@ -40,9 +41,48 @@ export class HSStatsAppComponent  implements OnInit{
       }
 
     }.bind(this));
+    setTimeout(function(){   this.initIsotope(); }.bind(this), 5000);
   }
 
   ngOnInit() {
     this.getInfos();
   }
+
+  initIsotope(){
+        // quick search regex
+        var qsRegex;
+
+        // init Isotope
+        var $grid = $('.grid').isotope({
+          itemSelector: '.element-item',
+          layoutMode: 'fitRows',
+          filter: function() {
+            return qsRegex ? $(this).text().match( qsRegex ) : true;
+          }
+        });
+
+        // use value of search field to filter
+        var $quicksearch = $('.quicksearch').keyup( debounce( function() {
+          console.log("lol");
+          qsRegex = new RegExp( $quicksearch.val(), 'gi' );
+          $grid.isotope();
+        }, 200 ) );
+
+        // debounce so filtering doesn't happen every millisecond
+        function debounce( fn, threshold ) {
+          var timeout;
+          return function debounced() {
+            if ( timeout ) {
+              clearTimeout( timeout );
+            }
+            function delayed() {
+              fn();
+              timeout = null;
+            }
+            timeout = setTimeout( delayed, threshold || 100 );
+          }
+        }
+
+  }
+
 }
